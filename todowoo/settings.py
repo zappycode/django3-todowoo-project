19822816b5527b27e38b6e9e765a30693bc2a66e
kramstyles <mark.eke@decagon.dev>
2022-04-30 21:04:16 +0100

    added api to settings and urls

--- todowoo/settings.py
+++ todowoo/settings.py
@@ -37,7 +37,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # APPS
     'todo',
+    'api',
+
+
+    # PIP INSTALLS
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
--- todowoo/urls.py
+++ todowoo/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from todo import views
 
 urlpatterns = [
@@ -33,4 +33,7 @@ urlpatterns = [
     path('todo/<int:todo_pk>', views.viewtodo, name='viewtodo'),
     path('todo/<int:todo_pk>/complete', views.completetodo, name='completetodo'),
     path('todo/<int:todo_pk>/delete', views.deletetodo, name='deletetodo'),
+
+    # Api
+    path('api/v1/', include('api.urls'))
 ]
