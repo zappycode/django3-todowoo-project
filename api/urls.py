b2dd9560c62d38a1fdd9565a671a1b43cbe58f57
kramstyles <mark.eke@decagon.dev>
2022-04-30 23:53:56 +0100

    created todo list and create view

--- api/urls.py
+++ api/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('todos/', views.TodoCreate.as_view(), name='todo-create'),
     path('completed/', views.TodoCompletedList.as_view(), name='todo-completed'),
 ]
--- api/views.py
+++ api/views.py
@@ -1,4 +1,4 @@
-from rest_framework import generics, permissions
+from rest_framework import generics, permissions, response, status
 
 from . import serializers
 from todo.models import Todo
@@ -6,6 +6,24 @@ from todo.models import Todo
 
 class TodoCompletedList(generics.ListAPIView):
     serializer_class = serializers.TodoSerializer
-    queryset = Todo.objects.all()
-    permission_class = [permissions.IsAuthenticated]
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        todos = Todo.objects.filter(user=user, datecompleted__isnull=False).order_by('-datecompleted')
+        return todos
+
+
+class TodoCreate(generics.ListCreateAPIView):
+    serializer_class = serializers.TodoSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        todos = Todo.objects.filter(user=user, datecompleted__isnull=True)
+        return todos
+    
+    def perform_create(self, serializer): 
+        serializer.save(user=self.request.user)
+        
     
